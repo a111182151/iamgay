2c6142ddfff1185b6d60befda36bde7df6a9c75f
a111182151 <a111182151@nkust.edu.tw>
2026-06-01 01:11:50 +0800

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -61,7 +61,7 @@ export const profileData: ProfileData = {
   name: "陳宥為",
   title: "航海科專業 | 重機旅人 | 多語才俊",
   avatar: "https://cdn.phototourl.com/free/2026-05-25-67542554-0598-4a01-a435-98b1ae5f4f55.jpg",
-  avatar3D: "/src/assets/images/profile_3d_render_1779725236653.png",
+  avatar3D: "https://a111182151.github.io/iamgay/src/assets/images/profile_3d_render_1779725236653.png",
   motto: "愛你真的沒辦法 你家沒有阿爾法 ｜ 你懂海 海就會幫你，你懂懂猜 神明就會幫你",
   email: "a111182151@nkust.edu.tw",
   constellation: "射手座",
